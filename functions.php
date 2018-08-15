7055cc02dc7ebe944cbbcdf7040b9fcb13b7a815
Stephen Axtell <axtell.stephen@gmail.com>
2018-08-15 14:54:43 +0200

    Added interior page, added caching trick, added some links plus function for page title

--- functions.php
+++ functions.php
@@ -1,13 +1,20 @@
 <!-- this file is used to interact with the WordPress system -->
 <?php 
   function universityFiles() {
-
-    wp_enqueue_script('mainUniversityJs', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+    // microtime() is a trick for dealing with caching issues
+    wp_enqueue_script('mainUniversityJs', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
     // loading a stylesheet
     wp_enqueue_style('fontAwesome', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-    wp_enqueue_style('universityMainStyles', get_stylesheet_uri());
+    // microtime() is a trick for dealing with caching issues
+    wp_enqueue_style('universityMainStyles', get_stylesheet_uri(), NULL, microtime());
     wp_enqueue_style('fontAwesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   }
   // to add scripts, the second parameter is naming a function that must be called
   add_action('wp_enqueue_scripts', 'universityFiles');
+
+  function universityFeatures() {
+    add_theme_support('title-tag');
+  }
+
+  add_action('after_setup_theme', 'universityFeatures');
 
