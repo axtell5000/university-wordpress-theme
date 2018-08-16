f92d6d7be7de21f3b086ad6d63385d818bfcfbb9
Stephen Axtell <axtell.stephen@gmail.com>
2018-08-16 16:15:51 +0200

    adding sidenav and controlling whether we see it a lot

--- page.php
+++ page.php
@@ -15,17 +15,54 @@
 
     <div class="container container--narrow page-section">
 
-      <div class="metabox metabox--position-up metabox--with-home-link">
-        <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
-      </div>
-      
-      <!-- <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
-        <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
-        </ul>
-      </div> -->
+      <?php 
+        // Calculating the id umber of parent, first we get current page ID, then use ID to determine parent. 
+        // if 0 means false and there is no parent
+        $theParent = wp_get_post_parent_id(get_the_ID());
+        // If current page has a parent show breadcrumb
+        if ($theParent) { ?> 
+          <div class="metabox metabox--position-up metabox--with-home-link">
+            <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+          </div>
+        <?php }
+      ?>
+   
+      <?php
+        // checking if current page has children, will return an array of them if they have 
+        $testArray = get_pages(array(
+          'child_of' => get_the_ID()
+        ));
+        
+        // Checking if page has a parent or is a parent - $testArray will have id's if current page has children
+        // We are doing this check to determine if we should display the sidenav on the right. These stand alone adhoc pages should not have the sidenav on the right
+        if ($theParent or $testArray) { ?>
+        <div class="page-links">
+          <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
+          <ul class="min-list">
+            <?php 
+            // example of an associative array in php
+            // $animalSounds = array(
+            //   'cat' => 'meow',
+            //   'dog' => 'bark',
+            //   'pig' => 'onk'
+            // );
+
+            // echo $animalSounds['cat'];
+            
+            if ($theParent) {
+              $findChildrenOf = $theParent;
+            } else {
+              $findChildrenOf = get_the_ID();
+            }
+
+            wp_list_pages(array(
+              'title_li' => NULL,
+              'child_of' => $findChildrenOf
+            )); 
+            ?>
+          </ul>
+        </div>
+      <?php } ?>
 
       <div class="generic-content">
         <?php the_content(); ?>
